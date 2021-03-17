3bc36a17e6190fcd8ce36857807cbacbfa662534
ArchitGandotra <2020A1R120@mietjammu.in>
2021-03-17 17:04:05 +0530

    Sumoftwonumbers

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("Enter Number 1 : ");
+	scanf("%d",&a);
+	printf("Enter Number 2 : ");
+	scanf("%d",&b);
+	printf("Sum of %d and %d is %d",a,b,a+b);
+	return 0;
+}
