40f439da08651bd9fa3fa310cf02bb9ba0695021
ArchitGandotra <2020A1R120@mietjammu.in>
2021-03-18 17:53:07 +0530

    modified

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,5 +1,10 @@
-#include<stdio.h>
-int main(){
-	printf("Hello World");
-	return 0;
-}
+//Author Name: Archit Gandotra
+//Creation Date: 17th March 2021
+//Purpose: To Print Hello World program 
+#include<stdio.h> //Preprocessive directive to include input output functions header file
+//Start of the main function body
+int main(){ 
+	printf("Hello World!");//Printf function calling to print Hello World
+	return 0;//Return statement
+	}
+//End of the main body function
