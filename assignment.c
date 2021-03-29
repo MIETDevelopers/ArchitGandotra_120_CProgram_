d054918ccde81210c486167481784041682ed0e8
ArchitGandotra <2020A1R120@mietjammu.in>
2021-03-29 17:41:31 +0530

    assignment

---
+++ assignment.c
@@ -0,0 +1,5 @@
+#include<stdio.h>//Preprocessor directive to include input output function header file
+int main(){//Start of the main function body
+	printf("Hello World");
+	return 0;//Return statement
+}
