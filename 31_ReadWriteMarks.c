077ebc690b7d07cde86e656a4052d8d16e527e65
ArchitGandotra <2020A1R120@mietjammu.in>
2021-04-07 22:00:35 +0530

    ReadWriteMarks

--- 31_ReadWriteMarks.c
+++ 31_ReadWriteMarks.c
@@ -2,17 +2,17 @@
 //A C program to Program for reading and writing the student marks data to files.				
 #include <stdio.h>
 #include <stdlib.h>
-struct s {
+struct s{
 	//Creating a user defined data type using structures.
 	char name[50];
 	int RollNo;
 	int Marks;
 };
-int main() {
+int main(){
 	struct s a[10],b[10];
 	FILE *fptr;
 	int i;
-	fptr=fopen("E:\\Softwares\\C_Programs\\Ishav_160_C_Programs_Repository\\031_ReadWriteMarks.txt","wb"); //Opening the file from the location.
+	fptr=fopen("E:\\Softwares\\C_Programs\\Archit_120_CProgram\\031_ReadWriteMarks.txt","wb"); //Opening the file from the location.
 	for (i=0;i<1;++i) {
 		fflush(stdin);
 		//Scanning the entered Values.
@@ -26,7 +26,7 @@ int main() {
 	//Writing program.
 	fwrite(a,sizeof(a),1,fptr);
 	fclose(fptr);
-	fptr=fopen("E:\\Softwares\\C_Programs\\Ishav_160_C_Programs_Repository\\031_ReadWriteMarks.txt","rb");
+	fptr=fopen("E:\\Softwares\\C_Programs\\Archit_120_CProgram\\031_ReadWriteMarks.txt","rb");
 	fread(b,sizeof(b),1,fptr);
 	printf("\n Entered details are:\n");
 	for (i=0;i<1;++i) {
